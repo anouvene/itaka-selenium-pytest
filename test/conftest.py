26a55d3f4b67b6b2d69d69f0416e4216ec64825c
anouvene <nouveno@gmail.com>
2024-09-07 13:43:04 +0200

    conftest upgrade bis

--- test/conftest.py
+++ test/conftest.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-# import os
-# os.environ['WDM_SSL_VERIFY'] = '0'
+import os
+os.environ['WDM_SSL_VERIFY'] = '0'
 
 @pytest.fixture(scope="class")
 def driver():
@@ -15,8 +15,8 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    # driver = webdriver.Chrome(options=options)
+    # service = Service(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
